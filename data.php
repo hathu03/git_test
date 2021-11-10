af7f85e29a0c34b5861a1b7e34204467b5b9c7a7
hathu03 <nguyenthuha2411vmh@gmail.com>
2021-11-10 08:50:48 +0700

    test

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php
+    $student = array(
+        array('name' => "Tiep", "age" => 18, "address" => "Ba Tu"),
+        array('name' => "Nam", "age" => 20, "address" => "HA Noi"),
+        array('name' => "Luc", "age" => 30, "address" => "Du bai"),
+        array('name' => "Ha Thu Xinh Dep", "age" => 16, "address" => "Phu Tho"),
+        array('name' => "Dung", "age" => 17, "address" => "Hue"),
+    );
